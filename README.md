7423bd693033054b2e7f49d4b0c81073a5f84d1d
ievgen-kostiukevych <ievgenkostiukevych@ievgens-macbook-pro.home>
2019-04-02 01:16:12 +0200

    Initial public commit

--- README.md
+++ README.md
@@ -1,2 +1,148 @@
-# Google-Spreadsheet-Driven-Arista-eAPI-Automation-Demo
-A simple script that reads data from Google spreadsheet and uses it as a config source for Arista switches
+# Google Spreadsheet Driven Arista eAPI Automation Demo
+
+This script realizes the concept of "SDN - Spreadsheet Defined Network" (c) Anthony P. Kuzub, CBC/ Radio Canada
+
+The script uses Google Drive and Spreadsheet APIs to get the information that is propagated into the Arista switches using eAPI.
+It was written to solve one particular use case, but can be used as a demo of using a Google spreadsheet as a data source for switch configuration.
+It will also read some information from the switch and populate a separate tab of the spreadsheet.
+
+The functionality currently realised:
+
+- Reading the vlan list and vlan names from the spreadsheet and creating them in the switch
+
+- Reading interface descriptions and access vlans from the spreadsheet and setting them in the switch (only one vlan per interface can be set via the script, trunk ports are not affected)
+
+- Reading interfaces status and the mac address table from the switch and writing the data to the spreadsheet
+
+The script is using [oauth2client](https://github.com/googleapis/oauth2client) library for authentication and [gspread](https://github.com/burnash/gspread) to access Goggle APIs.
+The script is tailored for Arista switches and is using [pyeapi](https://github.com/arista-eosplus/pyeapi).
+
+The Google spreadsheet needs to follow the exact format.
+A sample spreadsheet can be found [HERE](https://docs.google.com/spreadsheets/d/12l3Q-th76AO2daifAsOwWcnlWg6zrYHp_pXvaQWuEgI/edit?usp=sharing)
+Please use "save as copy" to be able to edit.
+The demo spreadsheet was used to drive a vEOS instance, therefore it is only limited to 12 ports.
+More ports and vlans can be added to the spreadsheet.
+Ports of >10GbE and multi-lane ports are also supported (e.g. Ethernet7/3).
+
+User needs to supply the Google API json access token.
+Example token is provided, however it is a dummy file that will **not** give you access to any Google API resources.
+You **have** to provide your own API token.
+Follow the first step from this article as a how-to: [LINK](https://www.twilio.com/blog/2017/02/an-easy-way-to-read-and-write-to-a-google-spreadsheet-in-python.html)
+
+User also needs to suply a config file that has an **exact** name of the spreadsheet in your Google drive, IP address or a fully qualified domain name (FQDN) of the switch, and login and pass to the switch.
+
+Example config file is provided.
+
+```bash
+spreadsheet:Switches
+ip:172.16.158.100
+username:admin
+password:admin
+```
+
+## Requirements
+
+```bash
+pyeapi
+gspread
+oauth2client
+colorlog
+```
+
+## Installation
+
+Clone the repo.
+
+```bash
+git clone https://github.com/ievgen-kostiukevych/Google-Spreadsheet-Driven-Arista-eAPI-Automation-Demo.git
+```
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install dependencies.
+
+```bash
+pip install requirements
+```
+
+## Usage example
+
+Use `-h` for how-to use.
+
+```bash
+usage: automation.py [-h] [-vL] [-iD] [-iS] [-iV] [-c] config api [time]
+
+positional arguments:
+  config                provide the config file
+  api                   provide the API json file
+  time                  amount of seconds to wait before repeating tasks,
+                        default is 10 seconds
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -vL, --vlans_list     read vlans from the spreadsheet and create in the
+                        switch
+  -iD, --interfaces_description
+                        read interfaces descriptions from the spreadsheet and
+                        update in the switch
+  -iS, --interfaces_status
+                        read interfaces status from the switch and update in
+                        the spreadsheet
+  -iV, --interfaces_vlans
+                        read interfaces vlans from the spreadsheet and update
+                        in the switch
+  -c, --continious      repeat activated tasks after 10 seconds (default,
+                        unless time is specified)
+```
+
+Examples:
+
+```bash
+python automation.py -vL -iD -iS -iV -c demo_config.cfg switch-automation-api.json 30
+```
+
+Script will execute all tasks every 30 seconds using `switch-automation-api.json` Google API access token and `demo_config.cfg` config source file
+
+```bash
+python automation.py -iD demo_config.cfg switch-automation-api.json
+```
+
+Script will update interfaces descriptions once and will exit.
+
+```bash
+python automation.py -iV -c demo_config.cfg switch-automation-api.json
+```
+
+Script will update interfaces access vlans every 10 seconds (default timeout value).
+
+To stop the continious execution use keyboard interrupt `CTRL/CMD + C`
+
+## Contributing
+
+This project is a demo, it is not heavily maintained.
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## Release History
+
+- 0.1.0
+  - The first public release
+
+## TO DOs
+
+- Migrate from depricated `oauth2client` library
+
+## Meta
+
+Thanks you to Robert Welch (Arista Networks) and Anthony P. Kuzub (CBC/Radio Cannada) for support and inspiration
+
+## Licenses
+
+Author – [Ievgen Kostiukevych](https://github.com/ievgen-kostiukevych), European Broadcasting Union, Technology and Innovation
+
+[MIT](https://choosealicense.com/licenses/mit)
+
+[pyeapi](https://github.com/arista-eosplus/pyeapi) is [Copyright (c) 2015, Arista Networks EOS+ All rights reserved.](https://github.com/arista-eosplus/pyeapi/blob/develop/LICENSE)
+
+## Disclaimer
+
+The software is provided "As is", WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND.
+Use at your own risk.
+Lab testing is ALWAYS recomended before any use for production network.
---
+++ automation.py
@@ -0,0 +1,281 @@
+#!/usr/bin/env python
+
+import pprint
+import pyeapi
+from oauth2client.service_account import ServiceAccountCredentials
+import gspread
+import time
+import logging
+import colorlog
+import sys
+from datetime import datetime, timedelta
+import argparse
+
+__author__ = 'Ievgen Kostiukevych'
+__copyright__ = 'Copyright 2019, Ievgen Kostiukevych'
+
+# ===================Argument parser ===================
+
+parser = argparse.ArgumentParser()
+parser.add_argument('config', help='provide the config file')
+parser.add_argument('api', help='provide the API json file')
+parser.add_argument(
+    'time', help='amount of seconds to wait before repeating tasks, default is 10 seconds', nargs='?', default=10, type=int)
+parser.add_argument(
+    '-vL', '--vlans_list', help='read vlans from the spreadsheet and create in the switch', action='store_true')
+parser.add_argument('-iD', '--interfaces_description',
+                    help='read interfaces descriptions from the spreadsheet and update in the switch', action='store_true')
+parser.add_argument(
+    '-iS', '--interfaces_status', help='read interfaces status from the switch and update in the spreadsheet', action='store_true')
+parser.add_argument(
+    '-iV', '--interfaces_vlans', help='read interfaces vlans from the spreadsheet and update in the switch', action='store_true')
+parser.add_argument('-c', '--continious',
+                    help='repeat activated tasks after 10 seconds (default, unless time is specified)', action='store_true')
+
+
+args = parser.parse_args()
+configFile = args.config
+waitTime = args.time
+config = {}
+with open(configFile, 'r') as f:
+    for line in f:
+        (key, val) = line.split(':')
+        config[key] = val.strip('\n')
+
+
+# =================== Logger ===================
+
+# Creates basic logger
+logger = colorlog.getLogger()
+# Sets logging level (options: DEBUG, INFO, WARNING, ERROR, CRITICAL)
+logger.setLevel(colorlog.colorlog.logging.INFO)
+# Adds coloring to the logger messages
+handler = colorlog.StreamHandler()
+handler.setFormatter(colorlog.ColoredFormatter())
+logger.addHandler(handler)
+
+# ===================JSON pretty print ===================
+# Sets pretty print settings for JSON output to console
+pp = pprint.PrettyPrinter(indent=1, stream=None)
+
+# =================== Google sheet access ===================
+
+
+def googleAuthorize():
+    # Sets access to Google spreadsheets and Google drive
+    scope = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+    # Imports the JSON access token and extracts credential
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        args.api, scope)
+    # Athorization with extracted credentials
+    gc = gspread.authorize(credentials)
+    logger.info(' - Google Cloud API authorization successful')
+    return(gc)
+
+
+# Initial authorization at program start
+gc = googleAuthorize()
+
+# Extracts data from the spreadsheet
+switchConfigs = gc.open(config['spreadsheet'])
+vlanList = switchConfigs.worksheet('Vlan List')
+vlanPorts = switchConfigs.worksheet(
+    'Interfaces VLAN Allocation and Descriptions')
+interfacesMacTable = switchConfigs.worksheet('MAC addresses table')
+interfacesStatusTable = switchConfigs.worksheet('Interfaces status table')
+
+# =======================BEGIN FUNCTIONS DESCRIPTION======================
+
+
+def setInterfaceDescriptions():
+    # Reads interfaces description from the spreadsheet and writes to the switch
+    logger.info('===========================================')
+    logger.info(' - Updating interfaces descriprions...')
+    for port in vlanPorts.get_all_records():
+        portsDescription.set_description('Ethernet {}'.format(
+            port['Port']), value=port['Description'])
+    logger.info(' - Interfaces descriptions updated')
+
+
+def createVlans():
+    # Reads vlan list and names from the spreadsheet and writes to the switch
+    logger.info('===========================================')
+    logger.info(' - Updating vlan table...')
+    vlans = connectedSwitch.api('vlans')
+    vlans.autorefresh = True
+    for vlan in vlanList.get_all_records():
+        vlans.create(vlan['Vlan ID'])
+        vlans.set_name(vlan['Vlan ID'], name=vlan['Vlan Name'])
+    logger.info(' - Vlan table updated')
+
+
+def setInterfaceVlans():
+    # Reads interfaces vlans list from the spreadsheet and writes to the switch
+    logger.info('===========================================')
+    logger.info(' - Updating interface vlans...')
+    currentVlans = portsVlans.getall()
+    for port in vlanPorts.get_all_records():
+        try:
+            if str(port['Vlan']) != str(currentVlans['Ethernet{}'.format(port['Port'])]['access_vlan']):
+                logger.warning(
+                    '   - Modifying port {} - new access Vlan {}'.format(port['Port'], port['Vlan']))
+                portsVlans.set_access_vlan('Ethernet {}'.format(
+                    port['Port']), value=port['Vlan'])
+            else:
+                pass
+        except KeyError:
+            pass
+    logger.info(' - Interface vlans updated')
+
+
+def getInterfacesState():
+    # Reads interfaces statuses and mac address table from the switch and writes to the spreadsheet
+    logger.info('===========================================')
+    logger.info(' - Updating interfaces status table..')
+    getIterfaces = connectedSwitch.enable('show interfaces status')
+    interfacesStatus = getIterfaces[0]['result']['interfaceStatuses']
+    macAdrTable = connectedSwitch.enable('show mac address-table')
+    unicastMacAdrTable = macAdrTable[0]['result']['unicastTable']['tableEntries']
+
+    header = ['autoNegotiateActive', 'autoNegotigateActive', 'bandwidth',
+              'description', 'duplex', 'interfaceType', 'lineProtocolStatus', 'linkStatus', 'macAddress', 'entryType']
+    # Define the cell range
+    cellRange = interfacesStatusTable.range('A2:L256')
+
+    # Sort by interface name
+    interfaceList = []
+    for interface in interfacesStatus.keys():
+        interfaceList.append(interface)
+    interfaceList.sort()
+
+    # Flatten the list of dicts into a list of values in order
+    flattened_test_data = []
+    # ===================
+    for i in interfaceList:
+        for entry in unicastMacAdrTable:
+            for value in entry.values():
+                if value == i:
+                    interfacesStatus[i].update(
+                        {'macAddress': entry['macAddress']})
+                    interfacesStatus[i].update(
+                        {'entryType': entry['entryType']})
+    # ===================
+
+    for i in interfaceList:
+        flattened_test_data.append(i)
+        try:
+            flattened_test_data.append(
+                interfacesStatus[i]['vlanInformation']['vlanId'])
+        except KeyError:
+            flattened_test_data.append('N/A')
+        for j in header:
+            try:
+                flattened_test_data.append(interfacesStatus[i][j])
+            except KeyError:
+                flattened_test_data.append('N/A')
+    # Send flattened list to the cell range to be re-rendered as a table
+    for i, cell in enumerate(cellRange):
+        try:
+            cell.value = flattened_test_data[i]
+        except IndexError:
+            pass
+
+    interfacesStatusTable.update_cells(cellRange)
+    logger.info(' - Interfaces status table updated')
+
+
+def wait(message):
+    # Introduces delay between task repetition and emergency delay after API quota error
+    if message == 'api':
+        logger.error(
+            ' - API quota exceeded! Waiting for 60 seconds before restarting...')
+        for i in range(60, 0, -1):
+            sys.stdout.write(str(i)+' ')
+            sys.stdout.flush()
+            time.sleep(1)
+        sys.stdout.write('\n')
+    elif message == 'wait':
+        logger.info('===========================================')
+        logger.info(
+            ' - Tasks completed. Waiting for {} seconds before restarting...'.format(waitTime))
+        for i in range(waitTime, 0, -1):
+            sys.stdout.write(str(i)+' ')
+            sys.stdout.flush()
+            time.sleep(1)
+        sys.stdout.write('\n')
+
+# ====================================END FUNCTIONS DESCRIPTION====================
+
+# =================================== MAIN FUNCTION ==============================
+
+
+# First - connect to the switch and check connection and eAPI
+connect = pyeapi.client.connect(
+    transport='https', host=config['ip'], username=config['username'], password=config['password'])
+logger.info(
+    ' - Trying to connect to {} using provided username and password'.format(config['ip']))
+connectedSwitch = pyeapi.client.Node(connect)
+hostname = connectedSwitch.enable('show hostname')
+logger.info(' - Succsessfully connected to ' +
+            hostname[0]['result']['hostname'])
+version = connectedSwitch.enable('show version')
+logger.info(pp.pprint(version[0]['result']))
+# Second - read initial data from the switch
+vlans = connectedSwitch.api('vlans')
+portsVlans = connectedSwitch.api('switchports')
+portsDescription = connectedSwitch.api('interfaces')
+
+# Start executing selected tasks when continious flag is not set
+
+if args.vlans_list and not args.continious:
+    createVlans()
+
+if args.interfaces_description and not args.continious:
+    setInterfaceDescriptions()
+
+if args.interfaces_vlans and not args.continious:
+    setInterfaceVlans()
+
+if args.interfaces_status and not args.continious:
+    getInterfacesState()
+
+if not args.continious:
+    logger.info('===========================================')
+    logger.info(' - All tasks finished. Exiting...')
+
+# Tasks repetition when continious flag is set
+# Each task can be interrupted by the Google API exhaustion error. 1 minute wait is introduced.
+
+try:
+    while args.continious:
+        logger.info(' - ' + str(datetime.now().replace(microsecond=0)))
+        logger.info(' - Succsessfully connected to ' +
+                    hostname[0]['result']['hostname'])
+        if args.vlans_list:
+            try:
+                createVlans()
+            except gspread.exceptions.APIError:
+                wait('api')
+                gc = googleAuthorize()
+        if args.interfaces_description:
+            try:
+                setInterfaceDescriptions()
+            except gspread.exceptions.APIError:
+                wait('api')
+                gc = googleAuthorize()
+        if args.interfaces_vlans:
+            try:
+                setInterfaceVlans()
+            except gspread.exceptions.APIError:
+                wait('api')
+                gc = googleAuthorize()
+        if args.interfaces_status:
+            try:
+                getInterfacesState()
+            except gspread.exceptions.APIError:
+                wait('api')
+                gc = googleAuthorize()
+        wait('wait')
+except KeyboardInterrupt:
+    logger.info('Stopped')
